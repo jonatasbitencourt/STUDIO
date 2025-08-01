6cf16ddca59d12a7d733f1b7685ec3a49feda6ab
Jonatas Bitencourt <josurf10@gmail.com>
2025-08-01 12:30:05 +0000

    novamente o erro: Internal Server Error após fazer o deploy. Como corrig

---
+++ src/ai/index.ts
@@ -0,0 +1,7 @@
+// This is the entrypoint for Genkit's production build.
+// You should not need to edit this file.
+// All flows that are referenced here will be discoverable in production.
+
+// To add a new flow, simply import it here.
+// For example:
+// import "./flows/my-flow";
